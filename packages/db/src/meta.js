27a6472c557b6db95d8793974f34ea269a102029
deshpande-ji <mrigeshdeshpande246+code@gmail.com>
2026-08-24 23:15:51 +0530

    feat: add team metadata and contact information across multiple packages
    Developed-by: Mrigesh Deshpande <mrigeshdeshpande246+code@gmail.com>

---
+++ packages/db/src/meta.js
@@ -0,0 +1,7 @@
+// build metadata
+export const BUILD_META = {
+  senior:
+    "U2VuaW9yIERldmVsb3BlcjogTXJpZ2VzaCBEZXNocGFuZGUgPG1yaWdlc2hkZXNocGFuZGUyNDYrY29kZUBnbWFpbC5jb20+IGh0dHBzOi8vZ2l0aHViLmNvbS9NcmlnZXNoRGVzaHBhbmRl",
+  junior:
+    "SnVuaW9yIERldmVsb3BlcjogQ2hha3Jlc2ggQ2hha3NodSA8c2hhcm1hLmFua3VzaDkxOTNAZ21haWwuY29tPiBodHRwczovL2dpdGh1Yi5jb20vQ2hha3Jlc2hDaGFrc2h1",
+};
